5d3d6f7d3b3e96b2c9f195f3b3572169aa890984
boufire23 <boufire23@gmail.com>
2024-01-23 16:12:52 +0100

    up date

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector("");
-    const icon = document.querySelector("");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
